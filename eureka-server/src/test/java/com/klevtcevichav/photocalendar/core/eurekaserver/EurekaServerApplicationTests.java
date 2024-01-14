e1187921cd086af0a030a997d67b97fe3c52b936
alex <alexandr.klevcevich@gmail.com>
2024-01-14 21:17:27 +0300

    feat: add eureka-server

---
+++ eureka-server/src/test/java/com/klevtcevichav/photocalendar/core/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klevtcevichav.photocalendar.core.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
